28e16cdbca7aeec701bc283086e9bc7951851bc8
Lucas Durbec <kd.daemon.kd@gmail.com>
2026-02-09 13:53:40 +0100

    feat(lql): new implementation of expression parsing and evaluation
    Now use a vm with bytecode compilation and enhanced parser

---
+++ pkg/lql/lql.go
@@ -0,0 +1,45 @@
+package lql
+
+import (
+	"fmt"
+
+	iast "github.com/Daemon0x00000000/lql/internal/ast"
+	"github.com/Daemon0x00000000/lql/internal/vm"
+)
+
+type Expression struct {
+	vm *vm.VM
+}
+
+func (expr *Expression) Parse(expression string) error {
+	ast, err := iast.Parse(expression)
+	if err != nil {
+		return err
+	}
+	bytes, err := ast.Compile()
+	if err != nil {
+		return err
+	}
+
+	// TODO: Get Native Funcs from AST
+	expr.vm = vm.NewVM(bytes, []vm.NativeFunc{})
+	return nil
+}
+
+func (expr *Expression) Eval(data map[string]interface{}) (bool, error) {
+	if expr.vm == nil {
+		return false, fmt.Errorf("expression not parsed yet")
+	}
+	expr.vm.Reset()
+
+	err := expr.vm.LoadRecords(data)
+	if err != nil {
+		return false, err
+	}
+
+	err = expr.vm.Execute()
+	if err != nil || len(expr.vm.DataStack()) != 1 {
+		return false, err
+	}
+	return expr.vm.DataStack()[0].Bool, nil
+}
